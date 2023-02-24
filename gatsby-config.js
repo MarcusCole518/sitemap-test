92f1f7f576ba1620b678284a395f813f40da083a
Marcus Little <marcuslittle@marcuss-mbp-2.lan>
2023-02-24 16:44:12 -0500

    adding sitemap plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    siteUrl: `https://sitemaptest46951.gatsbyjs.io/`,
   },
   plugins: [
     `gatsby-plugin-image`,
@@ -39,5 +39,6 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-sitemap`
   ],
 }
